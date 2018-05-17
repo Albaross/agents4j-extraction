b6300c38c768d3d0faaadf7d3c085d3db6059899
Barbi <Manuel.Barbi@adesso.de>
2018-05-17 17:53:10 +0200

    added filters

---
+++ src/main/java/org/albaross/agents4j/extraction/filters/TooSpecificRulesFilter.java
@@ -0,0 +1,12 @@
+package org.albaross.agents4j.extraction.filters;
+
+import org.albaross.agents4j.extraction.Filter;
+import org.albaross.agents4j.extraction.KnowledgeBase;
+
+public class TooSpecificRulesFilter<A> implements Filter<A> {
+
+    @Override
+    public KnowledgeBase<A> apply(KnowledgeBase<A> kb) {
+        return kb;
+    }
+}
