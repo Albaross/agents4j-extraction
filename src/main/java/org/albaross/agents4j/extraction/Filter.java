b8624893c847563245abd8fd42f7d16c31da6b67
Barbi <Manuel.Barbi@adesso.de>
2018-05-22 10:07:30 +0200

    Removed Filter and Xtractor

--- src/main/java/org/albaross/agents4j/extraction/Filter.java
+++
@@ -1,7 +0,0 @@
-package org.albaross.agents4j.extraction;
-
-import org.albaross.agents4j.extraction.KnowledgeBase;
-
-import java.util.function.Function;
-
-public interface Filter<A> extends Function<KnowledgeBase<A>, KnowledgeBase<A>> { }
