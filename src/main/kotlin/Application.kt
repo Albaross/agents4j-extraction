bb538427795ad502f19caa683d701d096925d49e
Barbi <Manuel.Barbi@adesso.de>
2018-07-21 14:21:52 +0200

    added main method and first test

---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,5 @@
+package org.albaross.agents4j.extraction
+
+fun main(args: Array<String>) {
+    println("It works!")
+}
---
+++ src/test/kotlin/ApplicationTest.kt
@@ -0,0 +1,12 @@
+package org.albaross.agents4j.extraction
+
+import org.junit.Test
+
+class ApplicationTest {
+
+    @Test
+    fun testEquality() {
+        TODO()
+    }
+
+}
